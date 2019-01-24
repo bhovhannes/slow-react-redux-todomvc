e231accce4bd4cdb3b3390e9f33424fd3f0c601b
Hovhannes Babayan <bhovhannes@gmail.com>
2019-01-25 03:33:42 +0400

    refactor: tweak config

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,3 +1,32 @@
 module.exports = {
-  use: ['@neutrinojs/jest', '@neutrinojs/react']
+  use: [
+    '@neutrinojs/jest',
+    [
+      '@neutrinojs/react',
+      {
+        /* preset options */
+
+        // Example: disable Hot Module Replacement
+        hot: false,
+
+        // Target specific browsers with @babel/preset-env
+        targets: {
+          browsers: ['last 1 Chrome versions']
+        },
+
+        // Add additional Babel plugins, presets, or env options
+        babel: {
+          // Override options for @babel/preset-env:
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                useBuiltIns: false
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  ]
 };
